a2353368ea70b717074ccf03e3fd6e2354e2c7a2
lSatoshil <1057047858@qq.com>
2019-05-13 19:22:05 +0800

    gs-consuming-rest
    消费其他服务

---
+++ Getting Started Guides/gs-consuming-rest/src/main/java/hello/Application.java
@@ -0,0 +1,40 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+//    public static void main(String[] args) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random",Quote.class);
+//        logger.info(quote.toString());
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+        //RestTemplate 支持 HTTP 类型的请求，如 get put post delete 
+        return args -> {
+            Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            logger.info(quote.toString());
+        };
+    }
+}
