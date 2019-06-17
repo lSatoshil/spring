a3ad4b9dd7510d5ba542543e5938a37281c1c9ac
lSatoshil <1057047858@qq.com>
2019-06-17 22:14:35 +0800

    add

---
+++ Getting Started Guides/gs-caching/src/main/java/hello/BookRepository.java
@@ -0,0 +1,6 @@
+package hello;
+
+public interface BookRepository {
+
+    Book getByIsbn(String isbn);
+}
---
+++ Getting Started Guides/gs-crud-with-vaadin/src/main/java/hello/Customer.java
@@ -0,0 +1,51 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Customer() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id,
+                firstName, lastName);
+    }
+}
